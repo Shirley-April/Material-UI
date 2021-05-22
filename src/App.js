2a75ac7c87fc6eb4c048308c9c396f919c087e96
Shirley-April <shirleyapril23@gmail.com>
2021-05-22 12:09:34 +0300

    created another table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MTable from "./components/MTable"
+import MTable2 from './components/MTable2'
 
 function App() {
   return (
     <div>
        <MTable/>
+       <MTable2/>
     </div>
   );
 }
